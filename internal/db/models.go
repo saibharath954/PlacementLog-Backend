50ad484fb302f239446747f105038ea453d66737
varnit-ta <varnitsingh65@gmail.com>
2025-07-12 02:09:02 +0530

    modularized post data type

--- internal/db/models.go
+++ internal/db/models.go
@@ -8,7 +8,17 @@ type User struct {
 }
 
 type Post struct {
-	ID       string
-	UserID   string
-	PostBody json.RawMessage
+	ID       string          `json:"id"`
+	UserID   string          `json:"user_id"`
+	PostBody json.RawMessage `json:"post_body"`
+}
+
+type Round struct {
+	Content string `json:"content"`
+}
+
+type PostBody struct {
+	Company string  `json:"company"`
+	Role    string  `json:"role"`
+	Rounds  []Round `json:"rounds"`
 }
